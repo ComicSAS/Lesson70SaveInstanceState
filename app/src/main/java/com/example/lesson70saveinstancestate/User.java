a5ecadb4d704bab039fe7dad3b79d0d8c2ca1977
Sukhovolskij.Andrew <sukhovolskijandrewoid@gmail.com>
2019-11-17 22:35:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lesson70saveinstancestate/User.java
@@ -0,0 +1,17 @@
+package com.example.lesson70saveinstancestate;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+}
